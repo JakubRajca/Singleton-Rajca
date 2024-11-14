334a5c933124879350376327847d94255d0e38fc
Jakub Rajča <94049646+JakubRajca@users.noreply.github.com>
2024-11-14 09:38:44 +0100

    14.11

--- obj/Debug/net8.0/Singleton.AssemblyInfo.cs
+++ obj/Debug/net8.0/Singleton.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e70eec928bd92e5f3591522cafbfecce2afff37b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Singleton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
